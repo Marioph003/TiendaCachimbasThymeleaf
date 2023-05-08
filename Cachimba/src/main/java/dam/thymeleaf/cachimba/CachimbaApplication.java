4d1b6fbbe077d5086d21ea4bee76833003c2dd08
Marioph003 <mpraher0306@g.educaand.es>
2023-05-08 11:29:38 +0200

    Primer commit

---
+++ Cachimba/src/main/java/dam/thymeleaf/cachimba/CachimbaApplication.java
@@ -0,0 +1,13 @@
+package dam.thymeleaf.cachimba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachimbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachimbaApplication.class, args);
+	}
+
+}
---
+++ Cachimba/src/test/java/dam/thymeleaf/cachimba/CachimbaApplicationTests.java
@@ -0,0 +1,13 @@
+package dam.thymeleaf.cachimba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachimbaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
